c0bb6c5accd966f5fa4a23f294681c22e053a6fb
agu2012 <674314531@qq.com>
2020-08-08 13:38:15 +0800

    初始化数据库配置

---
+++ app/src/main/java/km/gxy/com/funtest/db/Province.java
@@ -0,0 +1,39 @@
+package km.gxy.com.funtest.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * 省
+ *
+ * @Project FunWeather
+ * @author xiayi.gu@2020/8/8
+ */
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
